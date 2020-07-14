468f1219782ff44dbbe39029fa6a63e326e8ac12
IonutDragosVLAD <IonutDragosVLAD@9.145.28.181>
2020-07-14 16:27:51 +0300

    Initial commit

---
+++ src/test/java/com/summerSchool/firstProject/MyFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summerSchool.firstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
